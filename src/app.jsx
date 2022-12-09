f8ddfbfaad43798383caff7dfafd226aebfc5326
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-12-08 23:33:07 -0500

    PropTypes

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,19 @@
 import Painting from "./components/painting"
 
 export default function App() {
+// const isOnline = true
     return (
         <div>
+            {/* {isOnline && 'Online'}
+            {isOnline ? 'Online' : 'Offline'} */}
+
+            {/* {[1, 2, 3, 4, 5].map(el => (
+                <div>{el}</div>
+            ))} */}
+
             <Painting title={"Sasha"} price={5} />
+
+
         </div>
     )
 } 
--- src/components/painting.jsx
+++ src/components/painting.jsx
@@ -1,15 +1,22 @@
+import PropTypes from 'prop-types'
+
 export default function Painting(props) {
   const {title, price} = props
   return (
     <div>
       <img src="" alt="" width="480" />
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <p>
         Author: <a href=""></a>
       </p>
-      <p>Price: {props.price} $</p>
+      <p>Price: {price} $</p>
       <p>Avilabilety: ......</p>
       <button type='button' >Add to chart</button>
     </div>
   )
+}
+
+Painting.propTypes = {
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
 }
